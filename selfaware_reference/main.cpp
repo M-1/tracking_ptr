4649cb98c0537ba1d5886a97974d1d01808e94f1
a <a@a.local>
2015-08-04 20:33:54 +0200

    Initial Commit

---
+++ selfaware_reference/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  selfaware_reference
+//
+//  Created by a on 04/08/15.
+//  Copyright (c) 2015 a. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
